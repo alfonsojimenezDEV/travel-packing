dcd8b95d65845efab6b943e9dfac504ccd14c409
Alfonso Jiménez <aljimeruz@gmail.com>
2022-03-09 19:29:45 +0100

    Item - sin terminar pag72

---
+++ src/Item.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {blurOnKey} from './util'
+
+    export let item; // parámetro que recibirá el componente y que se corresponde con el item a pintar.
+                     // este item tendrá será un objeto con varias propiedades como name y packed
+    let editing = false; 
+    
+</script>  
+
+
+<li>
+    <input type="checkbox" bind:value={item.packed} />
+    {#if editing}
+        <input bind:value={item.name} on:blur={() => (editing = false)}  type="text" />
+    {:else}
+        <span class="packed-{item.packed} on:click={() => (editing = true)}">{item.name}</span>
+    {/if}
+    <button class="icon">&#x1F5D1</button>
+</li>
+
+<style>
+    /* falta añadir el style de la página 72 */
+</style>
